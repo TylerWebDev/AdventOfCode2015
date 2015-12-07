3a0978690abf4fee4c8a2ed91400748f0307c943
Joseph Walker <joseph@gravity4.com>
2015-12-06 20:15:53 -0600

    Part 2, day 6, Haskell

--- Day6/Haskell/lights.hs
+++ Day6/Haskell/lights.hs
@@ -14,16 +14,25 @@ data Light
     = On
     | Off
     deriving (Eq, Show)
-    
+
 invert :: Light -> Light
 invert On = 
     Off
 invert Off = 
     On
+    
+decrement :: Int -> Int -> Int
+decrement a b 
+    | b - a >= 0 = b - a
+    | otherwise  = 0
 
 inverseOfRange :: [Int] -> Vector Light -> [(Int, Light)]
 inverseOfRange range lights =
     zip range $ map (invert . (lights !)) range
+
+alterRange :: (Int -> Int) -> [Int] -> Vector Int -> [(Int, Int)]
+alterRange alteration range lights =
+    zip range $ map (alteration . (lights !)) range
 
 rowColToIndex :: (Int, Int) -> Int
 rowColToIndex (row,col) =
@@ -32,6 +41,10 @@ rowColToIndex (row,col) =
 grid :: Vector Light
 grid =
     fromList $ map (const Off) ([0..(uncurry (*) gridDimensions - 1)] :: [Int])
+    
+fancyGrid :: Vector Int
+fancyGrid =
+    Vector.map (const 0) grid
 
 -- Create a row/col range from two points that we can then convert to indeces
 -- (499, 499) (500, 500)
@@ -49,12 +62,30 @@ changeLightGrid originalGrid instruction =
             TurnOff -> originalGrid // zip range (repeat Off)
             Toggle  -> originalGrid // inverseOfRange range originalGrid
             
+changeLightGridFancy :: Vector Int -> Instruction -> Vector Int
+changeLightGridFancy originalGrid instruction =
+    let range = map rowColToIndex $ createRowColRange (from instruction) (to instruction) in
+        case change instruction of
+            TurnOn  -> originalGrid // alterRange (+1) range originalGrid
+            TurnOff -> originalGrid // alterRange (decrement 1) range originalGrid
+            Toggle  -> originalGrid // alterRange (+2) range originalGrid
+            
 -- Implementations
+
+getInput :: IO (Maybe [Instruction])
+getInput =
+    mapM parseInstruction . lines <$> readFile "input.txt"
 
 partOne :: IO ()
 partOne = do
-    input <- mapM parseInstruction . lines <$> readFile "input.txt"
+    input <- getInput
     case input of
         Just instructionList -> print (length $ Vector.filter (== On) $ foldl changeLightGrid grid instructionList)
         Nothing -> print "Error"
     
+partTwo :: IO ()
+partTwo = do
+    input <- getInput
+    case input of
+        Just instructionList -> print $ Vector.foldl (+) 0 (foldl changeLightGridFancy fancyGrid instructionList)
+        Nothing -> print "Error"
