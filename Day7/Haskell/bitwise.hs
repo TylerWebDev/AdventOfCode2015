d28d23bb7bb707766207f0d15c9d043e80154672
Joseph Walker <joseph@gravity4.com>
2015-12-07 22:05:23 -0600

    Completed Day7 Part2 in Haskell

--- Day7/Haskell/bitwise.hs
+++ Day7/Haskell/bitwise.hs
@@ -1,13 +1,28 @@
-module Main where
+module Advent where
 
 import Wires
 import Parse
 import Text.Parsec
 import Data.Map as Map
 
-main :: IO ()
-main = do
-    input <- mapM (parse wireParser "Invalid Wire") <$> lines <$> readFile "input.txt"
+readInput :: IO (Either ParseError [(String, Wire String)])
+readInput =
+    mapM (parse wireParser "Invalid Wire") <$> lines <$> readFile "input.txt"
+
+partOne :: IO ()
+partOne = do
+    input <- readInput
     case input of
         Right wires -> print $ lookupEval (Map.fromList wires) "a"
-        Left _ -> print "Failed to parse wire diagram"
+        Left _      -> print "Failed to parse wire diagram"
+
+partTwo :: IO ()
+partTwo = do
+    input <- readInput
+    case input of
+        Right wires
+            -> case lookupEval (Map.fromList wires) "a" of
+                Just a  -> print $ lookupEval (Map.insert "b" (Const a) (Map.fromList wires)) "a"
+                Nothing -> print "Failed to evaluate 'a'"
+        Left _
+            -> print "Failed to parse wire diagram"
