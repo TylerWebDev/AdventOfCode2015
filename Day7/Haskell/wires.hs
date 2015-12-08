2d07bdd6a8386b0b44992937dce453a2016462a8
Joseph Walker <joseph@gravity4.com>
2015-12-07 21:56:20 -0600

    Renamed functions to be more clear in Day7 Haskell

--- Day7/Haskell/wires.hs
+++ Day7/Haskell/wires.hs
@@ -31,7 +31,7 @@ emptyCache =
 
 lookupEval :: Map.Map String (Wire String) -> String -> Maybe Int
 lookupEval m =
-    me
+    eval
         where
             e :: String -> Maybe Int
             e k =
@@ -39,17 +39,17 @@ lookupEval m =
                     Just (Const a)
                         -> Just a
                     Just (Link a)
-                        -> lookupEval m a
+                        -> eval a
                     Just (And a b)
-                        -> App.liftA2 (.&.) (me a) (me b)
+                        -> App.liftA2 (.&.) (eval a) (eval b)
                     Just (Or a b)
-                        -> App.liftA2 (.|.) (me a) (me b)
+                        -> App.liftA2 (.|.) (eval a) (eval b)
                     Just (LShift a i)
-                        -> App.liftA2 shift (me a) (Just i)
+                        -> App.liftA2 shift (eval a) (Just i)
                     Just (RShift a i)
-                        -> App.liftA2 shift (me a) $ Just (negate i)
+                        -> App.liftA2 shift (eval a) $ Just (negate i)
                     Just (Not a)
-                        -> complement <$> me a
+                        -> complement <$> eval a
                     Nothing
                         -> Just $ read k
-            me = memoize e
+            eval = memoize e
