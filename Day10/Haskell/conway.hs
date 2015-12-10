954a62b3e66bbc1d8df269189632f3fd377cae15
Joseph Walker <joseph@gravity4.com>
2015-12-09 23:39:12 -0600

    Cleaned up Day10 code

--- Day10/Haskell/conway.hs
+++ Day10/Haskell/conway.hs
@@ -7,15 +7,11 @@ say str =
     let appendHead s = (++ [head s]) in
         concatMap (\i -> appendHead i . show . length $ i) $ group str
 
-sayForeverFrom :: String -> [String]
-sayForeverFrom =
-    iterate say
-
 getNthSayIteration :: Int -> String
 getNthSayIteration n =
     -- We have to give n + 1 as the argument because the first result of 'iterate'
     -- is the identity case, e.g. the 41st element is the 40th application
-    last . take (n + 1) $ sayForeverFrom "1321131112"
+    last . take (n + 1) $ iterate say "1321131112"
 
 -- Implementations
 
